ff198b8f431b0aea3ed1a6d9cb6dcf164d6fb6ce
Rennan Alves <rennan_areas@hotmail.com>
2022-01-20 15:24:58 -0300

    Initial Commit

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHOU: ESPERADO: {esperado}, OBTIDO: {obtido}");
+            }
+            Console.ForegroundColor = cor;
+        }
+        private static void LeilaoComVariosLances()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+            
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+
+
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,47 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    class LeilaoTestes
+    {
+        public void LeilaoComVariosLances()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        public void LeilaoComApenasUmLance()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+    }
+}
