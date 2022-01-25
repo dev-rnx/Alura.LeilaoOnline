8c6c13f03b0a6bc987aa0683e50710083bd13132
Rennan Alves <rennan_areas@hotmail.com>
2022-01-25 01:20:00 -0300

    Update Aula02

---
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Theory]
+        [InlineData(4, new double[] {800, 400, 300, 100})]
+        [InlineData(2, new double[] { 800, 900 })]
+        public void NaoPermiteNovosLances(int qtdeEsperada, double[] ofertas)
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            leilao.TerminaPregao();
+
+            // Act
+            leilao.RecebeLance(fulano, 1000);
+
+            // Assert
+            var qtdeEsperado = qtdeEsperada;
+            var qtdeObtida = leilao.Lances.Count();
+            Assert.Equal(qtdeEsperado, qtdeObtida);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,139 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        /*
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComTresClientes()
+        {
+            // Arrange
+            // Dado (GIVEN) leilão com 3 clientes e lances realizados por eles
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("Beltrano", leilao);
+
+            leilao.RecebeLance(beltrano, 1400);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Act
+            // Quando (WHEN) o leilão/pregão termina 
+            leilao.TerminaPregao();
+
+            // Assert
+            // Então (THEN) o valor esperado é o maior lance dado pelo cliente
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        }
+        */
+
+        // Substitui os 4 testes acima
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })] // LeilaoComVariosLances / LeilaoComLancesOrdenadosPorValor
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })] // LeilãoDesordenados
+        [InlineData(800, new double[] { 800 })] // LeilaoComApenasUmLance
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double[] ofertas)
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+           
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLances()
+        {
+            // Arrange
+            // Dado (GIVEN) leilão com 3 clientes e lances realizados por eles
+            var leilao = new Leilao("Van Gogh");
+
+            // Act
+            // Quando (WHEN) o leilão/pregão termina 
+            leilao.TerminaPregao();
+
+            // Assert
+            // Então (THEN) o valor esperado é o maior lance dado pelo cliente
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
